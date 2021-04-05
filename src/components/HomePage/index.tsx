935f708716e9bc6e59cd1dca1fd53bb9a558f944
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-05 20:31:55 +0100

    added handling for dead endpoints (eg 49)

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -3,51 +3,66 @@ import PokemonCard from "../PokemonCard";
 import { StyledContainer } from "./style";
 
 interface PokemonVerbose {
-  species: species;
+  species?: species;
   order: number;
-  types: types;
-  abilities: abilities;
-  stats: stats;
-  sprites: sprites;
+  types?: types;
+  abilities?: abilities;
+  stats?: stats;
+  sprites?: sprites;
 }
 type Pokemon = species;
 
+let uuid = 999999;
+
 const HomePage = (): JSX.Element => {
   const [pokemonData, setPokemonData] = useState<PokemonVerbose[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
+
   useEffect(() => {
     const fetchRequiredPokemonData = async (): Promise<void> => {
+      setLoading(true);
       const result = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/?limit=151"
+        "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20"
       );
       const pokedexListData = await result.json();
       const { results } = pokedexListData;
       const pokeList: Array<PokemonVerbose> = results.map(
         async (poke: Pokemon) => {
           const { url } = poke;
-          const result = await fetch(url);
-          const pokedexData = await result.json();
-          const {
-            species,
-            order,
-            types,
-            abilities,
-            stats,
-            sprites
-          } = pokedexData;
-          return { species, order, types, abilities, stats, sprites };
+          try {
+            const result = await fetch(url);
+            const pokedexData = await result.json();
+            const {
+              species,
+              order,
+              types,
+              abilities,
+              stats,
+              sprites
+            } = pokedexData;
+            return { species, order, types, abilities, stats, sprites };
+          } catch {
+            return {
+              order: uuid++
+            };
+          }
         }
       );
-      console.log(await Promise.all(pokeList));
       setPokemonData(await Promise.all(pokeList));
+      setLoading(false);
     };
     fetchRequiredPokemonData();
   }, []);
 
   return (
     <StyledContainer>
-      {pokemonData.map(pokemon => (
-        <PokemonCard pokemon={pokemon} key={pokemon.order} />
-      ))}
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        pokemonData.map(pokemon => (
+          <PokemonCard pokemon={pokemon} key={pokemon.order} />
+        ))
+      )}
     </StyledContainer>
   );
 };
