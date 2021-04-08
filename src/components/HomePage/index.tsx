13f833b5ad5ab31e60ca37b0ad62f1ee455efd45
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-08 11:59:06 +0100

    trivial handler to view different poke generations

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -2,20 +2,32 @@ import React, { useEffect, useState } from "react";
 import PokemonCard from "../PokemonCard";
 import Spinner from "../Spinner";
 import { StyledContainer } from "./style";
+import { generations } from "../../data/generations";
 
-type Pokemon = species;
-
+type Pokemon = Species;
+type generation = {
+  number?: number;
+  offset?: number;
+  limit?: number;
+};
 let uuid = 999999;
 
 const HomePage = (): JSX.Element => {
   const [pokemonData, setPokemonData] = useState<PokemonVerbose[]>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
+  const [genNumber, setGenNumber] = useState<number>(1);
+
+  const generation: generation =
+    generations.find(gen => gen.number === genNumber) || {};
+
+  const offset = generation?.offset;
+  const limit = generation?.limit;
 
   useEffect(() => {
     const fetchRequiredPokemonData = async (): Promise<void> => {
       setLoading(true);
       const result = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/?limit=151"
+        `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
       );
       const pokedexListData = await result.json();
       const { results } = pokedexListData;
@@ -46,12 +58,11 @@ const HomePage = (): JSX.Element => {
           }
         }
       );
-      console.log(await Promise.all(pokeList));
       setPokemonData(await Promise.all(pokeList));
       setLoading(false);
     };
     fetchRequiredPokemonData();
-  }, []);
+  }, [offset, limit]);
 
   return (
     <>
@@ -59,9 +70,25 @@ const HomePage = (): JSX.Element => {
         {isLoading ? (
           <Spinner />
         ) : (
-          pokemonData.map(pokemon => (
-            <PokemonCard pokemon={pokemon} key={pokemon.order} />
-          ))
+          <>
+            <div style={{ width: "100%" }}>
+              <button
+                type="button"
+                onClick={() => {
+                  if (genNumber <= 8) {
+                    setGenNumber(genNumber + 1);
+                  } else {
+                    setGenNumber(1);
+                  }
+                }}
+              >
+                Next Generation
+              </button>
+            </div>
+            {pokemonData.map(pokemon => (
+              <PokemonCard pokemon={pokemon} key={pokemon.order} />
+            ))}
+          </>
         )}
       </StyledContainer>
     </>
