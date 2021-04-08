8f61d74924f20bfa106ef25eb6ab32480578a49f
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-08 22:24:31 +0100

    temporary fix to ref issue

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -18,7 +18,7 @@ type PokemonCardProps = {
 };
 
 const PokemonCard = forwardRef(
-  ({ pokemon }: PokemonCardProps, ref): JSX.Element => {
+  ({ pokemon }: PokemonCardProps, ref: any): JSX.Element => {
     const history = useHistory();
     const { species, id, types, sprites } = pokemon;
     const { name } = species;
@@ -37,27 +37,29 @@ const PokemonCard = forwardRef(
       typeColors.find(color => color.name === typeTwo)?.color || "#A8A878";
 
     return (
-      <StyledCard ref={ref} onClick={() => history.push(`/pokemon/${name}`)}>
-        {sprites?.front_default && (
-          <StyledImg
-            src={sprites.other?.["official-artwork"].front_default}
-            alt={species?.name}
-          />
-        )}
-        <StyledCardHead colorOne={colorOne} colorTwo={colorTwo}>
-          <StyledPokeball src={pokeball} alt="pokeball" />
-        </StyledCardHead>
-        <StyledCardBody>
-          <StyledCardBodyHeader>
-            <StyledCardBodyHeaderName>
-              {`${name.charAt(0).toUpperCase() + name.slice(1)}: ${id}`}
-            </StyledCardBodyHeaderName>
-            <StyledCardBodyHeaderType>
-              {typeOne === typeTwo ? typeOne : `${typeOne} / ${typeTwo}`}
-            </StyledCardBodyHeaderType>
-          </StyledCardBodyHeader>
-        </StyledCardBody>
-      </StyledCard>
+      <>
+        <StyledCard ref={ref} onClick={() => history.push(`/pokemon/${name}`)}>
+          {sprites?.front_default && (
+            <StyledImg
+              src={sprites.other?.["official-artwork"].front_default}
+              alt={species?.name}
+            />
+          )}
+          <StyledCardHead colorOne={colorOne} colorTwo={colorTwo}>
+            <StyledPokeball src={pokeball} alt="pokeball" />
+          </StyledCardHead>
+          <StyledCardBody>
+            <StyledCardBodyHeader>
+              <StyledCardBodyHeaderName>
+                {`${name.charAt(0).toUpperCase() + name.slice(1)}: ${id}`}
+              </StyledCardBodyHeaderName>
+              <StyledCardBodyHeaderType>
+                {typeOne === typeTwo ? typeOne : `${typeOne} / ${typeTwo}`}
+              </StyledCardBodyHeaderType>
+            </StyledCardBodyHeader>
+          </StyledCardBody>
+        </StyledCard>
+      </>
     );
   }
 );
